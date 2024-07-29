e5c1e97b02dd3f10706391c05a96e3a266bd8e5b
ClimenteA <climente.alin@gmail.com>
2024-07-29 16:29:14 +0300

    feat: better settings orm

--- ui/src/components/PFADataForm.tsx
+++ ui/src/components/PFADataForm.tsx
@@ -3,7 +3,6 @@ import dayjs from "dayjs";
 import { PlusLg, Save } from "react-bootstrap-icons";
 import { useForm, SubmitHandler } from "react-hook-form";
 import { req } from "../utils";
-import { ArrowRight } from "react-bootstrap-icons";
 
 interface IFormInput {
   id?: number;
@@ -28,6 +27,13 @@ export function PFADataForm() {
   const [caenSecundar, setCaenSecundar] = useState<string[]>([]);
   const [actualizatLa, setActualizatLa] = useState("");
 
+  function humanDate(date?: string) {
+    if (date) {
+      const formattedDate = dayjs(date).format("DD-MM-YYYY");
+      setActualizatLa(formattedDate);
+    }
+  }
+
   useEffect(() => {
     req
       .get("/v1/setari/date-pfa")
@@ -51,12 +57,7 @@ export function PFADataForm() {
           setValue("email", pfaData.email);
           setValue("iban", pfaData.iban);
           setValue("caenPrincipal", pfaData.caenPrincipal);
-          if (pfaData.actualizat_la) {
-            const formattedDate = dayjs(pfaData.actualizat_la).format(
-              "DD-MM-YYYY"
-            );
-            setActualizatLa(formattedDate);
-          }
+          humanDate(pfaData.actualizat_la);
         }
       })
       .catch(function (error) {
@@ -75,7 +76,8 @@ export function PFADataForm() {
       .then(function (response) {
         setLoading(false);
         if (response.status == 200) {
-          setSaveStatus(response.data.detail || "");
+          humanDate(response.data.actualizat_la);
+          setSaveStatus("Datele au fost salvate!");
         }
       })
       .catch(function (error) {
@@ -217,7 +219,8 @@ export function PFADataForm() {
 
         <div style={{ marginTop: "2rem" }}>
           <button type="submit" disabled={loading}>
-            <Save /> Salveaza datele PFA
+            <Save /> {" "}
+            Salveaza datele PFA
           </button>
           <p className="pico-color-zinc-450 text-center">
             {loading && saveStatus.length > 0 ? "Se salveaza..." : null}
